311c6fb2ccf7fbc060a49520269eeaf85a85403d
gyeongent <59150207+gyeongent@users.noreply.github.com>
2022-10-17 15:42:12 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import logo from './logo.svg';
+import logo from './img/ls_logo.svg';
+import InsLogo from './img/instagram_logo.svg';
+import TumblLogo from './img/tumblbug_logo.svg';
 import './App.css';
 
 function App() {
@@ -6,17 +8,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          현재 페이지 준비중입니다.
+        </div>
+        <div className="social">
+        <ul>
+          <li>
+            <a href="https://instagram.com/life_saves._" target="blank">
+              <img src={InsLogo} alt="Instagram Icon" width="25px"/>
+            </a>
+          </li>
+          <li>
+            <a href="https://tumblbug.com/u/lifesaves" target="blank">
+              <img src={TumblLogo} alt="Tumblbug Icon" width="75px"/>
+            </a>
+          </li>
+        </ul>
+      </div>
       </header>
     </div>
   );
